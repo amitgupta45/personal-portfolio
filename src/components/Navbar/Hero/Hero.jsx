e1d2a32e28b4e489f3430cb664c610a5abe04057
amitgupta45 <amitgupta246828@gmail.com>
2024-08-11 00:42:26 +0530

    update

---
+++ src/components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <section>
+      <div>
+        <h1>Hi, I'm Amit</h1>
+        <p>I'm a front-end developer</p>
+        <a href="mailto:amitgupta245828@gmail.com">Contact Me</a>
+      </div>
+      <img src="assets/hero/amitphoto.jpg" alt="my image"></img>
+    </section>
+  );
+};
