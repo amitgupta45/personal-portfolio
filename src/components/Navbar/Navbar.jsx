90aff567629e2a3d2350ba2f74b2c9a76c86ba30
amitgupta45 <amitgupta246828@gmail.com>
2024-08-13 00:43:58 +0530

    Changes Made

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,27 @@
 import React, { useState } from "react";
+
 import styles from "./Navbar.module.css";
-// import Menuicon from "../../../assets/nav/menuicon.png";
-// import CloseIcon from "../../../assets/nav/CloseIcon.png";
+import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
-        Amit Gupta
+        Portfolio
       </a>
       <div className={styles.menu}>
-  <div className={styles.menuBtn} onClick={() => setMenuOpen(!menuOpen)}>
-    {menuOpen ? (
-      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 50 50">
-        <path fill="white" d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z"></path>
-      </svg>
-    ) : (
-      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 30 30">
-        <path fill="white" d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"></path>
-      </svg>
-    )}
-  {/* </div> */}
-</div>
-
-
-        {/* <img
+        <img
           className={styles.menuBtn}
-        //   src={menuOpen ? "./assets/nav/CloseIcon.png" : "./assets/nav/menuicon.png"}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
           alt="menu-button"
           onClick={() => setMenuOpen(!menuOpen)}
-        ></img> */}
+        />
         <ul
           className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
           onClick={() => setMenuOpen(false)}
@@ -39,10 +30,10 @@ export const Navbar = () => {
             <a href="#about">About</a>
           </li>
           <li>
-            <a href="skills">Skills</a>
+            <a href="#experience">Experience</a>
           </li>
           <li>
-            <a href="#project">Projects</a>
+            <a href="#projects">Projects</a>
           </li>
           <li>
             <a href="#contact">Contact</a>
